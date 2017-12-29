2a2f11d9d17195f2fdcfee5b09c1438f298a44e0
JWZY <psyjavan@gmail.com>
2017-12-28 19:01:54 -0500

    rebuild

---
+++ staging/assets/js/footer.js
@@ -0,0 +1,6 @@
+document.write('<!-- Footer --><footer id="footer"><div class="inner"><h3>Let's get in touch!</h3><ul class="actions"><li><a class="button alt icon fa-phone">416 877 2902</a></li><li><a href="mailto:khrysmar777@gmail.com" class="button alt icon fa-envelope-o">khrysmar777@gmail.com</a></li></ul><div class="copyright">
+&copy; 2018 Khrysmar School of Music.
+<p>Designed and built by <a href="javanwang.com/">Javan Wang.</a></p>
+</div>
+</div>
+</footer>')
